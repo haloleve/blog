362224698d50670cd70fbf9838d85a57814929a3
haloleve <abc973834183@163.com>
2022-09-07 16:06:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,6 @@ yarn docs:build
 cd docs/.vitepress/dist
 
 git init
-git config --global user.email "acb973834183@163.com"
-git config --global user.name "haloleve"
-git config --global init.defaultBranch main
-git branch -m master main
 git add -A
 git commit -m 'deploy'
 
